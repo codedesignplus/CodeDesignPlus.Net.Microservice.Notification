c7d12f06a32ae6795e07108090ea7cb03ea3ecbe
Wilzon <wliscano@codedesignplus.com>
2026-02-09 07:57:59 -0500

    feat: Enhance MainHub logging and update appsettings for logger level

--- src/entrypoints/CodeDesignPlus.Net.Microservice.Notification.gRpc/Hubs/MainHub.cs
+++ src/entrypoints/CodeDesignPlus.Net.Microservice.Notification.gRpc/Hubs/MainHub.cs
@@ -6,11 +6,17 @@ using Microsoft.AspNetCore.SignalR;
 namespace CodeDesignPlus.Net.Microservice.Notification.gRpc.Hubs;
 
 [Authorize]
-public class MainHub(IUserContext context) : Hub
+public class MainHub(IUserContext context, ILogger<MainHub> logger) : Hub
 {
     private const string TenantGroupPrefix = "Tenant";
     public override async Task OnConnectedAsync()
     {
+        logger.LogWarning("Client connected: {ConnectionId}", Context.ConnectionId);
+
+        //Write logger user
+        logger.LogWarning("User connected: {UserId}", context.IdUser);
+        logger.LogWarning("Tenant connected: {TenantId}", context.Tenant);
+
         if (context.Tenant != Guid.Empty)
             await Groups.AddToGroupAsync(Context.ConnectionId, $"{TenantGroupPrefix}:{context.Tenant}");
 
